4e7fb12dea7dc0e6b145c53a53533bf0d1ca74d9
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-24 23:24:37 +0530

    base code

---
+++ wrapper.py
@@ -0,0 +1,47 @@
+
+import sys
+
+from PySide6.QtCore import QUrl
+from PySide6.QtWebEngineCore import QWebEnginePage
+from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle('')
+
+        self.webEngineView = QWebEngineView()
+        self.setCentralWidget(self.webEngineView)
+        initialUrl = 'http://qt.io'
+        self.webEngineView.load(QUrl(initialUrl))
+        self.webEngineView.page().titleChanged.connect(self.setWindowTitle)
+        self.webEngineView.page().urlChanged.connect(self.urlChanged)
+
+    def load(self):
+        url = QUrl.fromUserInput(self.addressLineEdit.text())
+        if url.isValid():
+            self.webEngineView.load(url)
+
+    def back(self):
+        self.webEngineView.page().triggerAction(QWebEnginePage.Back)
+
+    def forward(self):
+        self.webEngineView.page().triggerAction(QWebEnginePage.Forward)
+
+    def urlChanged(self, url):
+        self.addressLineEdit.setText(url.toString())
+
+def startGUI():
+    app = QApplication(sys.argv)
+    mainWin = MainWindow()
+    availableGeometry = mainWin.screen().availableGeometry()
+    mainWin.resize(availableGeometry.width() * 2 / 3, availableGeometry.height() * 2 / 3)
+    mainWin.show()
+    sys.exit(app.exec())
+
+if __name__ == '__main__':
+    startGUI()
