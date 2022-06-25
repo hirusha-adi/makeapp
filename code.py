f863f41c75dcff6025b907c65aecf64e413e7ce1
Hirusha Adikari <36286877+hirusha-adi@users.noreply.github.com>
2022-06-25 10:48:56 +0530

    optimize code

--- code.py
+++ code.py
@@ -2,7 +2,6 @@
 import sys
 
 from PySide6.QtCore import QUrl
-from PySide6.QtWebEngineCore import QWebEnginePage
 from PySide6.QtWebEngineWidgets import QWebEngineView
 from PySide6.QtWidgets import QApplication, QMainWindow
 
@@ -11,29 +10,11 @@ class MainWindow(QMainWindow):
 
     def __init__(self):
         super().__init__()
-
         self.setWindowTitle('Scammer.info')
-
         self.webEngineView = QWebEngineView()
         self.setCentralWidget(self.webEngineView)
         initialUrl = 'http://scammer.info'
         self.webEngineView.load(QUrl(initialUrl))
-        self.webEngineView.page().titleChanged.connect(self.setWindowTitle)
-        self.webEngineView.page().urlChanged.connect(self.urlChanged)
-
-    def load(self):
-        url = QUrl.fromUserInput(self.addressLineEdit.text())
-        if url.isValid():
-            self.webEngineView.load(url)
-
-    def back(self):
-        self.webEngineView.page().triggerAction(QWebEnginePage.Back)
-
-    def forward(self):
-        self.webEngineView.page().triggerAction(QWebEnginePage.Forward)
-
-    def urlChanged(self, url):
-        self.addressLineEdit.setText(url.toString())
 
 def startGUI():
     app = QApplication(sys.argv)
